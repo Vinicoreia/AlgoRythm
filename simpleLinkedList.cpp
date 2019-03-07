b548f165109f0d09db1fc3ce0cf2fdbff48ecd17
vinicoreia <viniciusfilgf@gmail.com>
2019-03-07 00:13:02 -0300

    :notes: Kaleo - I Walk on Water

--- simpleLinkedList.cpp
+++ simpleLinkedList.cpp
@@ -8,6 +8,16 @@ typedef struct nameList {
 } nameList;
 
 
+
+void insert(nameList **l, string newName){
+    nameList *p; //Creates a new item
+    p = (nameList*) malloc(sizeof(nameList)); // Without allocating space and casting to nameList* we would get a Segmentation Fault.
+    p->name = newName; //Set its name
+    p->next = *l; //Make it point to what was previously the head of the list
+    *l=p;   //Assign p as the head of the list now, so this is a insertion at the begining of the list.
+
+}
+
 int main(){
 
     nameList a;
@@ -22,7 +32,10 @@ int main(){
     cout<< "Name in A: " << a.name<<endl;
     cout<< "Name in B through A: " << (*a.next).name<<endl;
 
+    nameList *head = &a;
+    insert(&head,"newHead");
 
+    std::cout<<"The Head now is: "<< (head->name);
 
 
     return 0;

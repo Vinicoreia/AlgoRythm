f1ef0867d3f6ec4557bd38bfb75591e453425f8a
Vinicoreia <viniciusfilgf@gmail.com>
2019-06-14 17:36:07 +0200

    :notes: Focus - Bazzi, 21 Savage

--- RemoveOutermostParentheses.py
+++ RemoveOutermostParentheses.py
@@ -4,7 +4,7 @@ class Solution:
         result = []
         for i in S:
             if i =='(':
-                opened.append(i)
+                opened.append(i) 
                 if len(opened)>1:
                     result.append(i)
             if i == ')':
@@ -14,4 +14,6 @@ class Solution:
         return "".join(result)
 
 
-# ((()))
+# Since the operation of appending to a list is done in O(1) time complexity and the
+# pop operation is done is in O(1) time as well.
+# So the time complexity for this code is O(n)

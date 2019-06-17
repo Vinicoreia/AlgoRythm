02a11a3bcd5edac77664a4d8b0a4ea1e28774502
Vinicoreia <viniciusfilgf@gmail.com>
2019-06-17 18:00:21 +0200

    :notes: Blame me - Melvin Alex

---
+++ LeetCodeEasy/mergeBinaryTrees.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1 or not t2:
+            return t1 or t2
+        else:
+            node = TreeNode(t1.val + t2.val)
+            node.left = self.mergeTrees(t1.left, t2.left)
+            node.right = self.mergeTrees(t1.right, t2.right)
+        return node

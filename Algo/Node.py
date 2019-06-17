118efac2486164af72beb82f31fdc0f683753137
Vinicoreia <viniciusfilgf@gmail.com>
2019-06-17 14:27:19 +0200

    :notes: Pray For Rain - Massive Atack

---
+++ Algo/Node.py
@@ -0,0 +1,30 @@
+import queue
+class Node:
+    def __init__(self, value = None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+a = Node(10)
+a.right = Node(2)
+a.left = Node(3)
+a.right.right = Node(5)
+a.right.left = Node(1)
+a.left.right =  Node(100)
+a.left.left = Node(6)
+
+
+
+def BFS(c):
+    Q = queue.Queue()
+    Q.put(c)
+    while(not Q.empty()):
+        p = Q.get()
+        print(p.value)
+        if(p.right != None):
+            Q.put(p.right)
+        if(p.left != None):
+            Q.put(p.left)
+
+
+BFS(a)

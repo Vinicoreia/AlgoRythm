9997b89756e2c813bd679c517609a947f6b64780
Vinicoreia <viniciusfilgf@gmail.com>
2019-07-02 13:55:39 +0200

    :notes: FKA twigs - Cellophane

--- LeetCodeEasy/parityArray.py
+++ LeetCodeEasy/parityArray.py
@@ -1,7 +1,20 @@
-
+from typing import List
 class Solution:
     def sortArrayByParity(self, A):
-        even = [i if i%2==0 for i in A]
-        odd = [o if o%2==1 for o in A]
+        even = [i for i in A if i%2==0]
+        odd = [o for o in A if o%2==1]
 
         return even+odd
+
+
+# New Solution using O(1) space
+    def even_odd(self, A: List[int]):
+        even, odd = 0, len(A)-1
+        while(even < odd):
+            if(A[even]%2 ==0):
+                even+=1
+            else:
+                A[even], A[odd] = A[odd], A[even]
+                odd-=1
+        return A
+

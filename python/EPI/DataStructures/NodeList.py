995d9963b0d152c7a0facd0f9f9e8ed4e707c304
Vinicoreia <viniciusfilgf@gmail.com>
2019-07-23 21:02:59 +0200

    :notes: Jorja Smith - Blue Lights

--- python/EPI/DataStructures/NodeList.py
+++ python/EPI/DataStructures/NodeList.py
@@ -2,3 +2,25 @@ class NodeList:
     def __init__(self, data=-1, nextP = None):
         self.data = data
         self.nextP = nextP
+
+
+class List:
+    def __init__(self, head=None):
+        self.head = NodeList()
+
+    def listFromArray(self, arr):
+        newNode = NodeList()
+        head = currentNode = self.head
+        for i in arr:
+            currentNode.nextP = NodeList(i)
+            currentNode = currentNode.nextP
+        self.head = head.nextP
+
+    def printValues(self):
+        currentNode = self.head
+        while currentNode:
+            print(currentNode.data)
+            currentNode = currentNode.nextP
+a = List()
+a.listFromArray([1,2,4,5,6])
+a.printValues()
